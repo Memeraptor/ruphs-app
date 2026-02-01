4fcd8980e86e47fca1801edb375823c7dee20e30
Augusto <4fouldeeds@gmail.com>
2026-01-31 19:21:42 -0500

    added middleware back

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+export const config = {
+  matcher: [
+    "/newcharacter",
+    "/classes/new",
+    "/races/new",
+    "/races/:id",
+    "/race-classes",
+    "/race-classes/new",
+    "/specializations/new",
+    "/characters/:id",
+  ],
+};
